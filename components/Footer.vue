f7d02aff642c1bbf1bbc0c4b4bfc05e7f70e33ca
Matt Miller <matt123miller@hotmail.co.uk>
2019-06-24 21:38:34 +0100

    made the footer super visible by copying something from the vue site.

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,12 +2,10 @@
   <div class="object-bottom">
     <!-- Find your icons from here - https://fontawesome.com/icons?d=gallery&s=brands -->
 
-    <div class="flex">
-      <div>hello</div>
-      <div>world</div>
-      <div>from</div>
-      <div>other</div>
-      <div>people</div>
+    <div class="flex bg-grey-lighter">
+      <div class="flex-1 text-grey-darker text-center bg-grey-light px-4 py-2 m-2">1</div>
+      <div class="flex-1 text-grey-darker text-center bg-grey-light px-4 py-2 m-2">2</div>
+      <div class="flex-1 text-grey-darker text-center bg-grey-light px-4 py-2 m-2">3</div>
     </div>
   </div>
 </template>
