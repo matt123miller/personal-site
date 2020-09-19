013905f9503d69e50f029cd3d6a8739e3aa39d15
Matt Miller <matt123miller@hotmail.co.uk>
2020-09-19 15:46:06 +0100

    fixing blog css a little

--- src/components/utils/FormatHtml/index.tsx
+++ src/components/utils/FormatHtml/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const FormatHtml: React.FC<Props> = ({ content }) => (
   <span
-    className="format-html"
+    className="format-html blog-post-container"
     dangerouslySetInnerHTML={{
       __html: content
     }}
