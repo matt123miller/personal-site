38fd15df5c4a7c2454d89333033582494e60bbd6
Matt Miller <matt123miller@hotmail.co.uk>
2020-10-24 15:01:48 +0100

    Added a file for constants like my various links etc,

---
+++ src/data/Constants/index.ts
@@ -0,0 +1,10 @@
+
+const github = "https://github.com/matt123miller";
+const twitter = "https://twitter/matt123miller";
+const email = "matt123miller@hotmail.co.uk";
+
+export const links = {
+  github,
+  twitter,
+  email
+}
