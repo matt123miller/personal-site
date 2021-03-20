edd38d3247fae42dfb79d951ae9f20fa91c45018
Matt Miller <matt123miller@hotmail.co.uk>
2021-03-20 18:02:44 +0000

    Better header, main, footer layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -25,18 +25,18 @@ const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
       <AnimatePresence exitBeforeEnter>
-        <main className="flex flex-col min-h-screen">
-          <Header siteTitle={data.site.siteMetadata.title} />
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <main className="flex flex-col main-content-container">
           <motion.div
             initial={{ y: 30, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ delay: 0.2 }}
-            >
+          >
             {children}
-            <Footer />
           </motion.div>
         </main>
+        <Footer />
       </AnimatePresence>
     </>
   );
